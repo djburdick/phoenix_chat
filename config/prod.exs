9650e6e53dbf08fbec8744b7b54aa5e15ea1738a
DJ Burdick <burdick.dj@gmail.com>
2017-04-09 16:30:19 -0700

    fix duplicate url config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :phoenix_chat, PhoenixChat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "young-sands-54503.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
@@ -31,11 +32,6 @@ config :phoenix_chat, PhoenixChat.Repo,
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-
-config :phoenix_chat, PhoenixChat.Endpoint,
-url: [scheme: "https", host: "young-sands-54503.herokuapp.com", port: 443],
-force_ssl: [rewrite_on: [:x_forwarded_proto]],
-
 #       https: [port: 443,
 #               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
 #               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
